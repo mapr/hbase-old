c52141652f8414174ffda2dddf7217118bab2bab
Aditya Kishore <aditya@maprtech.com>
2013-04-12 17:29:00 +0530

    Bug 8425 - you can set min versions greater than max

--- src/main/java/org/apache/hadoop/hbase/client/HBaseAdmin.java
+++ src/main/java/org/apache/hadoop/hbase/client/HBaseAdmin.java
@@ -718,6 +718,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void createTableAsync(HTableDescriptor desc, byte [][] splitKeys)
   throws IOException {
+    desc.validate();
     if (checkIfMapRTable(desc.getAlias(), true)) {
       maprHBaseAdmin_.createTable(desc, splitKeys);
       return;
@@ -1293,6 +1294,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void addColumn(byte [] tableName, HColumnDescriptor column)
   throws IOException {
+    column.validate();
     if (checkIfMapRTable(tableName, true)) {
       maprHBaseAdmin_.addColumn(Bytes.toString(tableName), column);
       return;
@@ -1364,6 +1366,7 @@ public class HBaseAdmin implements Abortable, Closeable {
    */
   public void modifyColumn(final byte [] tableName, HColumnDescriptor descriptor)
   throws IOException {
+    descriptor.validate();
     if (checkIfMapRTable(tableName, true)) {
       maprHBaseAdmin_.modifyColumn(Bytes.toString(tableName), descriptor);
       return;
